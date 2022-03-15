bb5d3edc339a6e40cae4e08ba96286bef4a60590
nadiaf85 <nadia.fiorio@alice.it>
2022-03-15 18:21:38 +0100

    installato bootstrap

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ Route::middleware('auth')
     ->group(function () {
         Route::get('/', 'HomeController@index')
         ->name('home');
-        Route::resource('/posts', 'PostController@index');
+        Route::resource('/posts', 'PostController');
     });
